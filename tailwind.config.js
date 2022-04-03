50c2cc1e31a8f0406b9c413f322bf906310731b8
wahyubudies <wahyubudies1@gmail.com>
2022-04-03 11:31:26 +0700

    hero and about section is done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['index.html'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#f59e0b',
+        secondary: '#64748b',
+        dark: '#0f172a'
+      },
+      screens: {
+        '2xl' : '1320px'
+      }
+    },
   },
   plugins: [],
 }
